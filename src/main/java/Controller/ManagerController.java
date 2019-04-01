ab46ead7e4135bcb983b197b302a44cba295ff21
Barbara Bochinska <bachna22@gmail.com>
2019-04-01 11:28:08 +0200

    Implemeted adding items by manager

--- src/main/java/Controller/ManagerController.java
+++ src/main/java/Controller/ManagerController.java
@@ -1,12 +1,13 @@
 package Controller;
 
-import Model.Manager;
-import Model.Warehouse;
+import Model.*;
 import View.ManagerView;
+import View.PrivilageMenuView;
 
 public class ManagerController {
 
     private Warehouse warehouse;
+    PrivilageMenuView privilageMenuView = new PrivilageMenuView();
     private ManagerView managerView = new ManagerView();
     private Manager manager;
 
@@ -26,14 +27,47 @@ public class ManagerController {
     private void chooseManagerAction(int managerAction) {
         switch (managerAction) {
             case 1:
+                managerView.showSecondOptionAdd();
+                int chosenSecondOption = managerView.getChosenOption();
+                chooseCategoryItemToAdd(chosenSecondOption);
                 break;
             case 2:
-                break;
-            case 3:
                 break;
             default:
                 throw new IllegalArgumentException("No such category.");
         }
+    }
+
+    private void chooseCategoryItemToAdd(int chosenSecondOption) {
+        String title = managerView.getUserTitle();
+        String author = managerView.getUserAuthor();
+        String length = managerView.getUserLength();
+        String releaseDate = managerView.getUserReleaseDate();
+        switch (chosenSecondOption) {
+            case 1:
+            case 2:
+            case 3:
+                createItem(title, author, length, releaseDate, chosenSecondOption);
+                showManagerView();
+                break;
+            default:
+                throw new IllegalArgumentException("No such option");
+
+        }
+    }
+
+    private void createItem(String title, String author, String length, String releaseDate, int category) {
+        MediaItem newItem = null;
+        if (category == 1) {
+            newItem = new Book(title, author, Integer.parseInt(length), Integer.parseInt(releaseDate));
+        }
+        else if (category == 2) {
+            newItem = new Movie(title, author, Integer.parseInt(length), Integer.parseInt(releaseDate));
+        }
+        else if (category == 3){
+            newItem = new Music(title, author, Integer.parseInt(length), Integer.parseInt(releaseDate));
+        }
+        manager.addItem(newItem);
     }
 
 
--- src/main/java/View/ManagerView.java
+++ src/main/java/View/ManagerView.java
@@ -26,6 +26,34 @@ public class ManagerView {
 
     public void showOptions() {
         System.out.println("\nOptions:");
-        System.out.println("1.Add item\n2.Delete item\n3.Back to privilage menu");
+        System.out.println("1.Add item\n2.Delete item");
+    }
+
+    public void showSecondOptionAdd() {
+        System.out.println("What item do you want to add:");
+        System.out.println("1.Book\n2.Movie\n3.Music");
+    }
+
+    public String getItemInfo() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        return input;
+    }
+
+    public String getUserTitle() {
+        System.out.println("Title:");
+        return getItemInfo();
+    }
+    public String getUserAuthor() {
+        System.out.println("Author:");
+        return getItemInfo();
+    }
+    public String getUserLength() {
+        System.out.println("Length:");
+        return getItemInfo();
+    }
+    public String getUserReleaseDate() {
+        System.out.println("Release Date:");
+        return getItemInfo();
     }
 }
