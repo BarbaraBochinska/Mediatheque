d79e94f1d73c18b364270d99ddc2862029a89bd6
Barbara Bochinska <bachna22@gmail.com>
2019-04-01 10:50:52 +0200

    Created method showManagerView and switch to select right option

--- src/main/java/Controller/ManagerController.java
+++ src/main/java/Controller/ManagerController.java
@@ -14,4 +14,27 @@ public class ManagerController {
         this.warehouse = warehouse;
         this.manager = new Manager(warehouse);
     }
+
+    void showManagerView() {
+        managerView.showManagerView();
+        managerView.showItems(warehouse.getAllItems());
+        managerView.showOptions();
+        int managerAction = managerView.getChosenOption();
+        chooseManagerAction(managerAction);
+    }
+
+    private void chooseManagerAction(int managerAction) {
+        switch (managerAction) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                throw new IllegalArgumentException("No such category.");
+        }
+    }
+
+
 }
