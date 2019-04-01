9bf15af0aadbab039c9a09bf0684fc58c63d47cb
Barbara Bochinska <bachna22@gmail.com>
2019-04-01 10:15:07 +0200

    Structure

--- src/main/java/Model/Manager.java
+++ src/main/java/Model/Manager.java
@@ -1,4 +1,13 @@
 package Model;
 
 public class Manager extends User {
+
+    public void addItem(MediaItem item) {
+    }
+
+    public void removeItem(MediaItem item) {
+    }
+
+    public void editItem(MediaItem item) {
+    }
 }
--- src/main/java/Model/Warehouse.java
+++ src/main/java/Model/Warehouse.java
@@ -1,4 +1,10 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Warehouse {
+
+    ArrayList<Book> books = new ArrayList<>();
+    ArrayList<Movie> movies = new ArrayList<>();
+    ArrayList<Music> music = new ArrayList<>();
 }
